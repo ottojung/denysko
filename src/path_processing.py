ba75f3bdfaab043b98eff2bd9abf21aff883c1b0
Vitaliy Mysak <vmysak@bccfe.ca>
2025-08-11 21:44:04 +0000

    save

--- src/path_processing.py
+++ src/path_processing.py
@@ -85,6 +85,9 @@ def rasterize_path(path, resolution=400):
     # Flip vertically to match coordinate system (matplotlib renders top-down, we want bottom-up)
     mask = np.flipud(mask)
     
+    # Also flip the y_grid to match the flipped mask
+    y_grid = np.flipud(y_grid)
+    
     return mask, x_grid, y_grid
 
 
