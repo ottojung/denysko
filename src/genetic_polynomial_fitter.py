ed820505a179023a84d011e9c4e4c705b3d0bad4
Vitaliy Mysak <vmysak@bccfe.ca>
2025-08-13 21:33:39 +0000

    save

--- src/genetic_polynomial_fitter.py
+++ src/genetic_polynomial_fitter.py
@@ -638,7 +638,7 @@ class GeneticPolynomialFitter:
                 distance = abs(pred - y)
                 min_distance = min(min_distance, distance)
 
-            total_distance += min_distance ** 0.3
+            total_distance += min_distance ** 0.5
 
         # Calculate base accuracy fitness
         average_distance = total_distance / len(data_points)
