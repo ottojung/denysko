c6fad2bce4cffdd78409f37175d32a97f73d1e01
Vitaliy Mysak <vmysak@bccfe.ca>
2025-08-16 00:25:22 +0000

    save

--- src/genetic_polynomial_fitter.py
+++ src/genetic_polynomial_fitter.py
@@ -284,7 +284,7 @@ class GeneticPolynomialFitter:
             # For each gene, just pick a random point index
             for gene_idx in range(self.num_genes):
                 random_point_idx = np.random.randint(0, len(data_points))
-                for point_idx in range(self.max_degree - 1):
+                for point_idx in range(self.max_degree):
                     solution.append(random_point_idx)
 
             population.append(solution)
